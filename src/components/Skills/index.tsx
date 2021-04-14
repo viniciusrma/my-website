2a3eb34dbd256622e4cf64c6b749520cfcd00969
Vinícius Alves <viniciusalves@7itec.online>
2021-04-14 16:23:38 -0300

    chore: implemented positioning on the skills section and some color tricks

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -4,14 +4,54 @@ import { Container } from './styles';
 const Skills: React.FC = () => {
   return (
     <Container>
-      <div className="page-header">
-        <h1 className="title">
-          Skills
-        </h1>
-      </div>
+      <header className="page-header">
+        <h1 className="title">Skills</h1>
+      </header>
 
+      <section className="skills">
+        
+        <div className="front-end" id="skill">
+          <h1 className="skill-title">Front-end</h1>
+          <div className="react" id="skill-id" />
+          <div className="angular" id="skill-id" />
+          <div className="typescript" id="skill-id" />
+          <div className="javascript" id="skill-id" />
+        </div>
+
+        <div className="back-end" id="skill">
+          <h1 className="skill-title">Back-end</h1>
+          <div className="nodejs" id="skill-id" />
+          <div className="nestjs" id="skill-id" />
+          <div className="typescript" id="skill-id" />
+        </div>
+
+        <div className="ui-library" id="skill">
+          <h1 className="skill-title">UI Library</h1>
+          <div className="ant-design" id="skill-id" />
+          <div className="material-ui" id="skill-id" />
+          <div className="bootstrap" id="skill-id" />
+        </div>
+
+        <div className="custom-ui" id="skill">
+          <h1 className="skill-title">Custom UI</h1>
+          <div className="styled-components" id="skill-id" />
+          <div className="sass" id="skill-id" />
+        </div>
+
+        <div className="database" id="skill">
+          <h1 className="skill-title">Database</h1>
+          <div className="mongodb" id="skill-id" />
+          <div className="mysql" id="skill-id" />
+          <div className="postgresql" id="skill-id" />
+        </div>
+
+        <div className="DevOps" id="skill">
+          <h1 className="skill-title">DevOps</h1>
+          <div className="docker" id="skill-id" />
+        </div>
+      </section>
     </Container>
-  )
-}
+  );
+};
 
 export default Skills;
--- src/components/Skills/styles.ts
+++ src/components/Skills/styles.ts
@@ -1,11 +1,31 @@
 import styled from 'styled-components';
+import reactjs from '../../assets/icons/react.svg';
+import angular from '../../assets/icons/angular.svg';
+import typescript from '../../assets/icons/typescript.svg';
+import javascript from '../../assets/icons/javascript.svg';
+import nodejs from '../../assets/icons/nodejs.svg';
+import nestjs from '../../assets/icons/nestjs.svg';
+import antdesign from '../../assets/icons/ant-design.svg';
+import materialui from '../../assets/icons/material-ui.svg';
+import styledcomponents from '../../assets/icons/styled-components.svg';
+import sass from '../../assets/icons/sass.svg';
+import bootstrap from '../../assets/icons/bootstrap.svg';
+import mongodb from '../../assets/icons/mongodb.svg';
+import mysql from '../../assets/icons/mysql.svg';
+import postgresql from '../../assets/icons/postgresql.svg';
+import docker from '../../assets/icons/docker.svg';
 
 export const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   width: 100%;
   background: pink;
-  display: flex;
   justify-content: center;
+  color: ${(props) => props.theme.colors.text};
+  background: ${(props) => props.theme.colors.background};
+  font-family: 'Nunito', sans-serif;
+  justify-content: center;
+  padding: 10px 200px;
+  margin-bottom: 50px;
 
   .page-header {
     height: 80px;
@@ -13,7 +33,97 @@ export const Container = styled.div`
     justify-content: center;
     align-items: center;
     display: flex;
-    color: ${props => props.theme.colors.text}; 
-    background: ${props => props.theme.colors.background}; 
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+
+  .skills {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .skill-title {
+    margin-right: 50px;
+  }
+
+  #skill {
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+    background: ${(props) => props.theme.colors.stroke};
+    padding: 10px 20px;
+    border-radius: 25px;
+    height: 15vh;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  }
+
+  #skill-id {
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin-right: 50px;
+    height: 70px;
+    width: 70px;
+  }
+
+  .react {
+    background-image: url(${reactjs});
+  }
+
+  .angular {
+    background-image: url(${angular});
+  }
+
+  .typescript {
+    background-image: url(${typescript});
+  }
+
+  .javascript {
+    background-image: url(${javascript});
+  }
+
+  .nodejs {
+    background-image: url(${nodejs});
+  }
+
+  .nestjs {
+    background-image: url(${nestjs});
+  }
+
+  .ant-design {
+    background-image: url(${antdesign});
+  }
+
+  .material-ui {
+    background-image: url(${materialui});
+  }
+
+  .styled-components {
+    background-image: url(${styledcomponents});
+  }
+
+  .sass {
+    background-image: url(${sass});
+  }
+
+  .bootstrap {
+    background-image: url(${bootstrap});
+  }
+
+  .mongodb {
+    background-image: url(${mongodb});
+  }
+
+  .mysql {
+    background-image: url(${mysql});
+  }
+
+  .postgresql {
+    background-image: url(${postgresql});
+  }
+
+  .docker {
+    background-image: url(${docker});
   }
 `;
